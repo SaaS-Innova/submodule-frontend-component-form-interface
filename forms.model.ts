ba1821a15aae60c04d7436f8b640e27adc67ae05
Nayan Gajera <nayan@saasinnova.com>
2025-12-25 15:38:26 +0530

    Add customIcon and isSetCustomIcon properties to IFormProps interface.

--- forms.model.ts
+++ forms.model.ts
@@ -32,6 +32,8 @@ export interface IFormProps {
     isShowAdjustButtons: boolean;
     isButtonInside?: boolean;
   };
+  isSetCustomIcon?: boolean;
+  customIcon?: JSX.Element | string;
 }
 
 export interface IDefaultTime {
